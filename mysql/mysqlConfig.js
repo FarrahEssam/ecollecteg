c20ba503d9f3231ce18b6aee0ddb93cc0912d038
andrew kadry <andrew.raghib@gmail.com>
2019-06-14 19:52:57 +0200

    final footer and database

--- mysql/mysqlConfig.js
+++ mysql/mysqlConfig.js
@@ -4,3 +4,31 @@ module.exports = {
     password: "ecollectpassword",//database password
     database: "ecollect" //database name
 };
+
+function queries() {
+    const individualTableQuery = "CREATE TABLE Individual (\n" +
+        "    ID int NOT NULL AUTO_INCREMENT,\n" +
+        "    FirstName varchar(255),\n" +
+        "    LastName varchar(255),\n" +
+        "    Gender varchar(255),\n" +
+        "    EWasteType varchar(255), \n" +
+        "    EWasteQuantity int, \n" +
+        "    AgeRange varchar(255), \n" +
+        "    PhoneNumber varchar(255), \n" +
+        "    Address varchar(255),\n" +
+        "    PRIMARY KEY (ID)" +
+        ");";
+    const companyTableQuery = "CREATE TABLE Company (\n" +
+        "    ID int NOT NULL AUTO_INCREMENT,\n" +
+        "    CName varchar(255),\n" +
+        "    EName varchar(255),\n" +
+        "    CompanyType varchar(255),\n" +
+        "    EWasteType varchar(255), \n" +
+        "    EWasteQuantity int, \n" +
+        "    CPhoneNumber varchar(255), \n" +
+        "    EPhoneNumber varchar(255), \n" +
+        "    Address varchar(255),\n" +
+        "    PRIMARY KEY (ID)" +
+        ");";
+
+}
--- routes/index.js
+++ routes/index.js
@@ -10,37 +10,6 @@ const con = mysql.createConnection({
     database: mysqlConfig.database
 });
 
-function queries() {
-    const individualTableQuery = "CREATE TABLE Individual (\n" +
-        "    ID int NOT NULL AUTO_INCREMENT,\n" +
-        "    FirstName varchar(255),\n" +
-        "    LastName varchar(255),\n" +
-        "    Gender varchar(255),\n" +
-        "    EWasteType varchar(255), \n" +
-        "    EWasteQuantity int, \n" +
-        "    AgeRange varchar(255), \n" +
-        "    PhoneNumber varchar(255), \n" +
-        "    Address varchar(255),\n" +
-        "    PRIMARY KEY (ID)" +
-        ");";
-    const companyTableQuery = "CREATE TABLE Company (\n" +
-        "    ID int NOT NULL AUTO_INCREMENT,\n" +
-        "    CName varchar(255),\n" +
-        "    EName varchar(255),\n" +
-        "    CompanyType varchar(255),\n" +
-        "    EWasteType varchar(255), \n" +
-        "    EWasteQuantity int, \n" +
-        "    CPhoneNumber varchar(255), \n" +
-        "    EPhoneNumber varchar(255), \n" +
-        "    Address varchar(255),\n" +
-        "    PRIMARY KEY (ID)" +
-        ");";
-
-    //con.query(individualTableQuery);
-    //con.query(companyTableQuery);
-    //console.log("database created");
-}
-
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
